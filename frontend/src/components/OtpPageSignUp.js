d6d4eb2116040fbd488e3c1bfb1e6d5503158b65
khushaalsareen <khushaalsareen@gmail.com>
2024-05-02 14:14:37 +0530

    resend otp button changes

--- frontend/src/components/OtpPageSignUp.js
+++ frontend/src/components/OtpPageSignUp.js
@@ -1,68 +1,81 @@
 import axios from 'axios';
-import React, { useState } from 'react'
+import React, { useState, useEffect,useRef } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function OtpPageSignUp() {
-    const [formData, setformData] = useState({
+    const [formData, setFormData] = useState({
         otp: '',
-        collegeId:localStorage.getItem('id')
-    })
+        collegeId: localStorage.getItem('id')
+    });
     const showToastMessage = () => toast("OTP resent successfully!!");
     const showToastMessage1 = () => toast("OTP verified successfully!!");
-    const obj = {collegeId:localStorage.getItem('id')}
+    const obj = { collegeId: localStorage.getItem('id') };
     const [err, setErr] = useState(false);
-    const [minutes,setMinutes] = useState(0);
-    const [seconds,setSeconds] = useState(0);
-    const buttonClass = seconds > 0 || minutes > 0 ? 'text-gray-200 border-none underline' : 'text-red-500 text-white border-none underline';
+    const [buttonDisabled, setButtonDisabled] = useState(true);
+    const timerRef = useRef(null);
 
     const handleClick = () => {
-        console.log(formData);
         axios.post(
-          "http://localhost:8000/api/update/verifyOTP",
-          formData, // Data object
-          {
-            headers: { "Content-Type": "application/json" }, // Headers
-          }
+            "http://localhost:8000/api/update/verifyOTP",
+            formData,
+            {
+                headers: { "Content-Type": "application/json" },
+            }
         )
-        .then((response) => {
-          if (response.data.statusCode === 200) {
-            //   localStorage.setItem('id', formData.collegeId);
-              setErr(false);
-              showToastMessage1();
-              setTimeout(()=>{
-                window.location.href = '/auth/login';
-              },1000)
-              
-            console.log("haa otp verified");
-          } else {
-            console.log("Error:", response.data.msg);
-          }
-        })
-        .catch((error) => {
-            setErr(true);
-          console.error("Error:", error);
-        });
-      }
-      const handleResend = ()=>{
-        axios.post(
+            .then((response) => {
+                if (response.data.statusCode === 200) {
+                    setErr(false);
+                    showToastMessage1();
+                    setTimeout(() => {
+                        window.location.href = '/auth/login';
+                    }, 1000);
+                    console.log("OTP verified");
+                } else {
+                    console.log("Error:", response.data.msg);
+                }
+            })
+            .catch((error) => {
+                setErr(true);
+                console.error("Error:", error);
+            });
+    }
+
+    const handleResend = () => {
+      axios.post(
           "http://localhost:8000/api/update/resendOtp",
-          obj, // Data object
+          obj,
           {
-            headers: { "Content-Type": "application/json" }, // Headers
+              headers: { "Content-Type": "application/json" },
           }
-        ).then((response) => {
+      ).then((response) => {
           if (response.data.statusCode === 200) {
-            showToastMessage()
-            console.log(" otp resent");
+              showToastMessage();
+              console.log("OTP resent");
+              setButtonDisabled(true);
+              clearInterval(timerRef.current); // Clear existing interval
+              timerRef.current = setInterval(() => {
+                  setButtonDisabled(false);
+              }, 30000); // Re-enable after 30 seconds
           } else {
-            console.log("Error:", response.data.message);
+              console.log("Error:", response.data.message);
           }
-        })
-        .catch((error) => {
-          console.error("Error:", error);
-        });
-      }
+      })
+          .catch((error) => {
+              console.error("Error:", error);
+          });
+  }
+  
+
+  useEffect(() => {
+    // Set up initial interval
+    timerRef.current = setInterval(() => {
+        setButtonDisabled(false);
+    }, 30000);
+    // Clear interval on component unmount
+    return () => clearInterval(timerRef.current);
+}, []);
+
     return (
         <div>
             <div className="flex items-center justify-center p-12 h-screen">
@@ -72,7 +85,7 @@ function OtpPageSignUp() {
 
                     <div className="my-10">
                         <div className="flex flex-col space-y-5">
-                            <label for="otp-box">
+                            <label htmlFor="otp-box">
                                 <p className="font-medium text-slate-700 pb-2">OTP</p>
                                 <input
                                     id="otp-box"
@@ -82,63 +95,49 @@ function OtpPageSignUp() {
                                     placeholder="OTP here"
                                     value={formData.otp}
                                     onChange={(e) => {
-                                      setformData({ ...formData, otp: e.target.value });
-                                  }}
+                                        setFormData({ ...formData, otp: e.target.value });
+                                    }}
                                 />
                             </label>
-                            {/* <div className='flex items-center justify-between'> */}
-                              {/* <p>
-                                Time Remaining: {" "}
-                                <span>
-                                  {minutes<10? `0${minutes}`: minutes}:{seconds<10 ? `0${seconds} : seconds`:seconds}
-                                </span>
-                              </p> */}
-
-                              {/* <button className={buttonClass} disabled={seconds > 0 || minutes > 0} onClick={handleResend}>
-                                Resend OTP
-                              </button>
-                            </div> */}
 
                             <button onClick={handleClick} className="w-full py-3 font-medium text-white bg-indigo-600 hover:bg-indigo-500 rounded-lg border-indigo-500 hover:shadow inline-flex space-x-2 items-center justify-center">
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
                                     fill="none"
                                     viewBox="0 0 24 24"
-                                    stroke-width="1.5"
+                                    strokeWidth="1.5"
                                     stroke="currentColor"
                                     className="w-6 h-6"
                                 >
                                     <path
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
                                         d="M15.75 5.25a3 3 0 013 3m3 0a6 6 0 01-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1121.75 8.25z"
                                     />
                                 </svg>
-
                                 <span>Verify the OTP</span>
                             </button>
 
-                            <button onClick={handleResend} className="w-full py-3 font-medium text-white bg-indigo-600 hover:bg-indigo-500 rounded-lg border-indigo-500 hover:shadow inline-flex space-x-2 items-center justify-center">
+                            <button onClick={handleResend} disabled={buttonDisabled} className={`w-full py-3 font-medium text-white bg-indigo-600 ${buttonDisabled ? 'cursor-not-allowed' : 'hover:bg-indigo-500'} rounded-lg border-indigo-500 hover:shadow inline-flex space-x-2 items-center justify-center`}>
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
                                     fill="none"
                                     viewBox="0 0 24 24"
-                                    stroke-width="1.5"
+                                    strokeWidth="1.5"
                                     stroke="currentColor"
                                     className="w-6 h-6"
                                 >
                                     <path
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
                                         d="M15.75 5.25a3 3 0 013 3m3 0a6 6 0 01-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1121.75 8.25z"
                                     />
                                 </svg>
-
                                 <span>Resend OTP</span>
                             </button>
                             <ToastContainer />
                         </div>
-                        { err && <p className=" text-red-600 font-medium inline-flex space-x-1 items-center">Incorrect OTP entered</p>}
+                        {err && <p className="text-red-600 font-medium inline-flex space-x-1 items-center">Incorrect OTP entered</p>}
                     </div>
                 </div>
             </div>
@@ -146,4 +145,4 @@ function OtpPageSignUp() {
     );
 }
 
-export default OtpPageSignUp
+export default OtpPageSignUp;
