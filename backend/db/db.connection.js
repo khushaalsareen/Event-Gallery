ffdaebeddbf0711811f980d94490ff30a7af42ce
khushaalsareen <khushaalsareen@gmail.com>
2024-07-10 20:24:59 +0530

    final code

--- backend/db/db.connection.js
+++ backend/db/db.connection.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const DatabaseConnection = async() => {
     try{
         await mongoose.connect("mongodb+srv://bhuvankaushal08112002:M8q85CYUkMJejSOJ@cluster0.0km1b1v.mongodb.net/events")
+        // await mongoose.connect("mongodb://127.0.0.1:27017/collegeEvents")
         console.log("Successfully connected to mongoDB")
     } catch(error){
         console.log(`ERROR: ${error.message}`)
